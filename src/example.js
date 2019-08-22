cdf500cb023021792c7a8f38e7a05e53ea45e1ed
Heunsig <heun3344@gmail.com>
2019-08-22 16:30:52 +0900

    changed code

--- src/example.js
+++ src/example.js
@@ -1,5 +1,4 @@
 let catchaUpload = new CatchaUpload(document.querySelector('#fileUpload'), {
-  // url: 'http://127.0.0.1:5000/upload'
   storageType: 's3',
   s3: {
     IdentityPoolId: 'ap-northeast-2:ea7a12a6-954d-43c0-9e11-72403ec0e280',
@@ -8,4 +7,4 @@ let catchaUpload = new CatchaUpload(document.querySelector('#fileUpload'), {
   }
 })
 
-console.log(catchaUpload.version)
+console.log('Catcha upload version: ', catchaUpload.version)
