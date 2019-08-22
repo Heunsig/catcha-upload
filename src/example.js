e5fd95af2b021ecf74b68ade5c17046b3431d603
Heunsig <heun3344@gmail.com>
2019-08-22 16:30:22 +0900

    version 0.0.21

--- src/example.js
+++ src/example.js
@@ -1,5 +1,11 @@
 let catchaUpload = new CatchaUpload(document.querySelector('#fileUpload'), {
-  url: 'http://127.0.0.1:5000/upload'
+  // url: 'http://127.0.0.1:5000/upload'
+  storageType: 's3',
+  s3: {
+    IdentityPoolId: 'ap-northeast-2:ea7a12a6-954d-43c0-9e11-72403ec0e280',
+    region: 'ap-northeast-2',
+    bucket: 'catcha-fileupload-demo'
+  }
 })
 
 console.log(catchaUpload.version)
